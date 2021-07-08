91a052d277febbb9c0be3ec87c0d7e7bcfeb12b6
Daven <dca120@sfu.ca>
2021-07-07 21:04:52 -0700

    added login error

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -136,6 +136,49 @@ public class Main {
     path = "/login",
     consumes = {MediaType.APPLICATION_FORM_URLENCODED_VALUE}
   )
+  public String handleBrowserLoginSubmit(Map<String, Object> model, User user) throws Exception {
+    try (Connection connection = dataSource.getConnection()) {
+      Statement stmt = connection.createStatement();
+      ResultSet rs = stmt.executeQuery("SELECT * FROM accounts");
+      ArrayList<User> output = new ArrayList<User>();
+      while (rs.next()) {
+        User tuser = new User();
+        String tname = rs.getString("username");
+        String tpassword = rs.getString("password");
+        int id = rs.getInt("id");
+        if(user.getUsername().equals(tname)){
+            if(user.getPassword().equals(tpassword)) {
+            //maybe put a user.IsLoggedIn() as a boolean?
+            //HOW TF WE GET THIS ON THE HTML?
+            System.out.println("LOGGED IN AS " + user.getUsername());
+            return "mainpage";
+            }else{
+                System.out.println("PASSWORD WRONG");
+                return "loginerror";
+            }
+        }
+        System.out.println("username not exist :(");
+      }
+      return "loginerror";
+    } catch (Exception e) {
+      model.put("message", e.getMessage());
+      return "error";
+    }
+  }
+
+  @GetMapping(
+    path = "/loginerror"
+  )
+  public String getLoginForm(Map<String, Object> model) {
+    User user = new User();  // creates new rectangle object with empty fname and lname
+    model.put("user", user);
+    return "loginerror";
+  }
+
+  @PostMapping(
+    path = "/loginerror",
+    consumes = {MediaType.APPLICATION_FORM_URLENCODED_VALUE}
+  )
   // stuff below this is temporary and wrong
   public String handleBrowserLoginSubmit(Map<String, Object> model, User user) throws Exception {
     try (Connection connection = dataSource.getConnection()) {
@@ -155,12 +198,12 @@ public class Main {
             return "mainpage";
             }else{
                 System.out.println("PASSWORD WRONG");
-                return "login";
+                return "loginerror";
             }
         }
         System.out.println("username not exist :(");
       }
-      return "login";
+      return "loginerror";
     } catch (Exception e) {
       model.put("message", e.getMessage());
       return "error";
